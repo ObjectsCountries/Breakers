6966e757cdb935244ab4d5f76f2d53fd1f00c8cd
ObjectsCountries <oandcofficial@gmail.com>
2023-10-11 14:37:49 -0700

    Set up module

---
+++ Assets/_breakersScript.cs
@@ -0,0 +1,13 @@
+﻿using Wawa.Modules;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+
+public class _breakersScript:ModdedModule{
+    void Start(){
+
+	}
+}
